2f57640811aa94e82930b916ca97d6f882b59dcd
deanturpin <git@lvbtn.uk>
2017-06-08 15:24:58 +0100

    paxman brand

--- tools/split-speech.sh
+++ tools/split-speech.sh
@@ -5,14 +5,14 @@ readarray lines < $1
 
 writetofile=blah.txt
 echo > paxman.txt
-echo > blair.txt
+echo > brand.txt
 
 for line in "${lines[@]}"; do
 
-	if [[ $line =~ PAXMAN ]]; then
+	if [[ $line =~ Paxman: ]]; then
 		writetofile=paxman.txt
-	elif [[ $line =~ BLAIR ]]; then
-		writetofile=blair.txt
+	elif [[ $line =~ Brand: ]]; then
+		writetofile=brand.txt
 	fi
 
 	echo $line >> $writetofile
