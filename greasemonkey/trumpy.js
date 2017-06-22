9bdf2f6d0e1a872809ed3fd9a382ecb213e56c04
deanturpin <git@lvbtn.uk>
2017-06-22 10:37:46 +0100

    implement uwr

--- greasemonkey/trumpy.js
+++ greasemonkey/trumpy.js
@@ -4,22 +4,41 @@
 // @description word stats
 // @include     http://0.0.0.0:8000/trumpasaurus/
 // @include     http://www.britishpoliticalspeech.org/*
+// @include     https://inbox.google.com/u/0/
 // @version     1
 // @grant       none
 // ==/UserScript==
 
 console.log("trumpy word stats")
 
-console.log("set up listener")
-
 // Add listen to catch on mouse up
 document.body.addEventListener('mouseup', function() { statistics() },false)
 
 function statistics() {
   
-  const speech = window.getSelection().toString()
-  const words = speech.split(" ")
+  const speech = window.getSelection().toString().toLowerCase()
+  const words = speech.split(/[ â€¢!"\#$%&()*+,\-./:;<=>?@\[\\\]^_`{|}~â€”â€“]+/)
+    
+	var uniqueWords = []
   
-  console.log(window.getSelection().toString())
-  console.log(words.length)
+	// Create/increment entry for each word
+	for (var i = 0; i < words.length; ++i) {
+
+			// Create entry if it doesn't exist, otherwise increment
+			uniqueWords[words[i]] == undefined
+				? uniqueWords[words[i]] = 1
+				: ++uniqueWords[words[i]]
+
+		}
+  
+  const totalWords = words.length
+  var uniqueWordCount = 0
+	for (var i in uniqueWords)
+		++uniqueWordCount
+
+  const uniqueWordRatio = (100 * uniqueWordCount / totalWords) 
+
+  console.log("Total words " + totalWords)
+	console.log("Unique words " + uniqueWordCount)
+  console.log("Unique word ratio " + uniqueWordRatio.toFixed(0) + "%")
 }
