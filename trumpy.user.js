a8ec19d4f6ad74fba3e6fd0b712fa96f920fd688
deanturpin <git@lvbtn.uk>
2017-06-23 14:22:53 +0100

    put the semicolons back :( after more to tampermonkey

--- trumpy.user.js
+++ trumpy.user.js
@@ -1,81 +1,74 @@
 // ==UserScript==
-// @name        Trump
-// @namespace   trump
-// @description word stats
-// @include     http://0.0.0.0:8000/trumpasaurus/
+// @name        Trumpy
+// @namespace   deanturpin
+// @description Quick word stat check to find candidate speeches for further study
 // @include     http://www.britishpoliticalspeech.org/*
-// @include     https://inbox.google.com/u/0/
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 
-var speech = ""
+console.log("trumpy!");
+
+var speech = "";
 
 // If there's a speech element then just use that
-const speechElement = document.getElementsByClassName("speech-content")[0]
+const speechElement = document.getElementsByClassName("speech-content")[0];
 
-// If we've found a speech store the text and analyse it 
-if (speechElement !== undefined){
-	
-	speech = speechElement.innerText
-  statistics()
+// If we've found a speech store the text and analyse it
+if (speechElement !== undefined) {
+    speech = speechElement.innerText;
+    statistics();
 }
-	
-/*
-// Otherwise set up an event listener
-if (speechElement === undefined)
-  document.body.addEventListener("contextmenu", function() { statistics() }, false)
-else {
-	speech = speechElement.innerText
-  statistics()
-}
-*/
 
 // Let's generate some stats!
 function statistics() {
-  
-  // const speech = window.getSelection().toString().toLowerCase()
-  const words = speech.split(/[ !"\#$%&()*+,\-./:;<=>?@\[\\\]^_`{|}~”“]+/)
-    
-	var uniqueWords = []
 
-	// Use associative array to remove duplicates
-	for (var i in words)
-		uniqueWords[words[i]] = 1
+    // const speech = window.getSelection().toString().toLowerCase()
+    const words = speech.split(/[ !"\#$%&()*+,\-./:;<=>?@\[\\\]^_`{|}~”“]+/);
 
-	// Word stats
-  const totalWords = words.length
-  var uniqueWordCount = 0
-	for (var i in uniqueWords)
-		++uniqueWordCount
+    var uniqueWords = [];
 
-  const uniqueWordRatio = (100 * uniqueWordCount / totalWords) 
+    // Use associative array to remove duplicates
+    for (var i in words)
+        uniqueWords[words[i]] = 1;
 
-  console.log("Total words " + totalWords)
-  console.log("Unique word ratio " + uniqueWordRatio.toFixed(0) + "%")
+    // Word stats
+    const totalWords = words.length;
+    var uniqueWordCount = 0;
 
-	// Summarise in a sentence
-	var summary = "The speech "
+    // Loop through the sparse array counting the entries
+    for (var j in uniqueWords)
+        ++uniqueWordCount;
 
-	// Interesting
-	summary += 
-		uniqueWordRatio < 25 ? "is repetitive and tiresome" :
-			uniqueWordRatio < 30 ? "is overly finessed" :
-				uniqueWordRatio < 33 ? "has had some work done" :
-					uniqueWordRatio < 36 ? "is practiced but loose" :
-						uniqueWordRatio < 42 ? "is spontaneous" : "is a rant"
+    // And calculate the important metric
+    const uniqueWordRatio = (100 * uniqueWordCount / totalWords);
 
-	// Length
-	summary += " "
-	summary += 
-		totalWords < 500 ? "and is very short" :
-			totalWords < 1000 ? "but at least it's short" :
-				totalWords < 2000 ? "and is on the short side" :
-					totalWords < 3000 ? "and is a good size" :
-						totalWords < 4000 ? "and is dragging on a bit" :
-  						totalWords < 5000 ? "and is getting long" :
-    						totalWords < 7000 ? "and is SOOO long" :
-       						totalWords < 9000 ? "... please to kill me now (far too long)" : "and is the longest I've seen"
-	
-	console.log(summary)
+    console.log("Total words " + totalWords);
+    console.log("Unique word ratio " + uniqueWordRatio.toFixed(0) + "%");
+
+    // Summarise in a sentence
+    var summary = "The speech ";
+
+    // Interesting
+    summary +=
+        uniqueWordRatio < 25 ? "is repetitive and tiresome" :
+    uniqueWordRatio < 30 ? "is overly finessed" :
+    uniqueWordRatio < 33 ? "has had some work done" :
+    uniqueWordRatio < 36 ? "is practiced but loose" :
+    uniqueWordRatio < 42 ? "is spontaneous" : "is a rant";
+
+    // Length
+    summary += " ";
+    summary +=
+        totalWords < 500 ? "and is very short" :
+    totalWords < 1000 ? "but at least it's short" :
+    totalWords < 2000 ? "and is on the short side" :
+    totalWords < 3000 ? "and is a good size" :
+    totalWords < 4000 ? "and is dragging on a bit" :
+    totalWords < 5000 ? "and is getting long" :
+    totalWords < 7000 ? "and is SOOO long" :
+    totalWords < 9000 ? "... please to kill me now (far too long)"
+    : "and is the longest I've seen";
+
+    console.log(summary);
 }
